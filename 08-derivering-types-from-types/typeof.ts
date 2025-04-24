106606856adf30de631ece0d3dd8820191fd473a
DanielCachro <danielcachro@gmail.com>
2025-04-24 18:15:23 +0200

    Lessons: 118-127, Derivering Types From Types - "typeof", "keyof", indexed access types and mapped types.

---
+++ 08-derivering-types-from-types/typeof.ts
@@ -0,0 +1,28 @@
+
+let userName = 'Max'
+
+
+type UserName = typeof userName
+
+
+const settings = {
+    difficulty: 'easy',
+    minLevel: 10,
+    didStart: false,
+    players: ['John', 'Jane']
+}
+
+
+
+
+
+
+type Settings = typeof settings
+
+
+function loadData(settings: Settings){
+    // ...
+}
+loadData(settings)
+
+
